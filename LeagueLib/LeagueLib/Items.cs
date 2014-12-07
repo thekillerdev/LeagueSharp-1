5cd03ec8148cd4a7c2971c02a239b0a79d59d1df
Trees <treesbot@gmail.com>
2014-12-07 13:43:16 -0500

    Add Buy and stuff

--- LeagueLib/LeagueLib/Items.cs
+++ LeagueLib/LeagueLib/Items.cs
@@ -22,31 +22,131 @@ namespace LeagueLib
             None
         }
 
+        public static Item GetItemByName(string name)
+        {
+            return items.FirstOrDefault(i => i.GetName().Equals(name));
+        }
+
+        public static Item GetItem(int itemID)
+        {
+            return items.FirstOrDefault(i => i.GetId() == itemID);
+        }
+
+        public class Item
+        {
+            private readonly int[] components;
+            private readonly int itemID;
+            private readonly string itemName;
+            private readonly float itemRange;
+            private readonly Tier itemTier;
+            private readonly int priceValue;
+            private readonly int sellValue;
+
+            public Item(int itemID,
+                string itemName,
+                Tier itemTier,
+                int priceValue,
+                int sellValue,
+                float itemRange,
+                int[] ucomponents = null)
+            {
+                this.itemID = itemID;
+                this.itemName = itemName;
+                this.itemTier = itemTier;
+                this.priceValue = priceValue;
+                this.sellValue = sellValue;
+                this.itemRange = itemRange;
+                components = ucomponents ?? new int[0];
+            }
+
+            public int GetId()
+            {
+                return itemID;
+            }
+
+            public void Buy()
+            {
+                ObjectManager.Player.BuyItem(GetItemId());
+            }
+
+            public ItemId GetItemId()
+            {
+                return (ItemId) itemID;
+            }
+
+            public string GetName()
+            {
+                return itemName;
+            }
+
+            public Tier GetTier()
+            {
+                return itemTier;
+            }
+
+            public int GetFullPriceValue()
+            {
+                return priceValue +
+                       components.SelectMany(i => items.Where(c => c.GetId() == i)).Sum(i2 => i2.GetPriceValue());
+            }
+
+            public int GetPriceValue()
+            {
+                return priceValue;
+            }
+
+            public int GetSellValue()
+            {
+                return sellValue;
+            }
+
+            public float GetRange()
+            {
+                return itemRange;
+            }
+
+            public int[] GetComponents()
+            {
+                return components;
+            }
+
+            public List<Item> GetComponentsList()
+            {
+                return (from item in pre_items from id in components where item.itemID == id select item).ToList();
+            }
+
+            public override string ToString()
+            {
+                return itemName + "[" + itemID + "]";
+            }
+        }
+
         private static readonly Item[] pre_items =
         {
-            new Item(3001, "Abyssal Scepter", Tier.Advanced, 980, 1708, 0f, new int[] {1033,1033,1026}),
-            new Item(3105, "Aegis of the Legion", Tier.Advanced, 820, 1330, 1100f, new int[] {1033,1028,1006}),
-            new Item(3113, "Aether Wisp", Tier.Advanced, 515, 665, 0f, new int[] {1052}),
+            new Item(3001, "Abyssal Scepter", Tier.Advanced, 980, 1708, 0f, new[] { 1033, 1033, 1026 }),
+            new Item(3105, "Aegis of the Legion", Tier.Advanced, 820, 1330, 1100f, new[] { 1033, 1028, 1006 }),
+            new Item(3113, "Aether Wisp", Tier.Advanced, 515, 665, 0f, new[] { 1052 }),
             new Item(1052, "Amplifying Tome", Tier.Basic, 435, 305, 0f),
             new Item(3301, "Ancient Coin", Tier.Basic, 365, 146, 0f),
-            new Item(3007, "Archangel's Staff (Crystal Scar)", Tier.Legendary, 1120, 1890, 0f, new int[] {3070, 1026}),
-            new Item(3003, "Archangel's Staff", Tier.Legendary, 1120, 1890, 0f, new int[] {3070, 1026}),
-            new Item(3504, "Ardent Censer", Tier.Legendary, 550, 1540, 0f, new int[] {3114, 3113}),
-            new Item(3174, "Athene's Unholy Grail", Tier.Legendary, 880, 1820, 0f, new int[]{3108, 3028}),
-            new Item(3005, "Atma's Impaler", Tier.Legendary, 780, 1610, 0f, new int[] {1031, 3093}),
-            new Item(3093, "Avarice Blade", Tier.Advanced, 400, 320, 0f, new int[] {1051}),
+            new Item(3007, "Archangel's Staff (Crystal Scar)", Tier.Legendary, 1120, 1890, 0f, new[] { 3070, 1026 }),
+            new Item(3003, "Archangel's Staff", Tier.Legendary, 1120, 1890, 0f, new[] { 3070, 1026 }),
+            new Item(3504, "Ardent Censer", Tier.Legendary, 550, 1540, 0f, new[] { 3114, 3113 }),
+            new Item(3174, "Athene's Unholy Grail", Tier.Legendary, 880, 1820, 0f, new[] { 3108, 3028 }),
+            new Item(3005, "Atma's Impaler", Tier.Legendary, 780, 1610, 0f, new[] { 1031, 3093 }),
+            new Item(3093, "Avarice Blade", Tier.Advanced, 400, 320, 0f, new[] { 1051 }),
             new Item(1038, "B. F. Sword", Tier.Basic, 1550, 1085, 0f),
-            new Item(3060, "Banner of Command", Tier.Legendary, 280, 2100, 0f, new int[] {3105, 3108}),
-            new Item(3102, "Banshee's Veil", Tier.Legendary, 1150, 1925, 0f, new int[] {3211, 1028}),
-            new Item(3254, "Berserker's Greaves - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new int[] {3006}),
-            new Item(3251, "Berserker's Greaves - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new int[] {3006}),
-            new Item(3253, "Berserker's Greaves - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new int[] {3006}),
-            new Item(3252, "Berserker's Greaves - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new int[] {3006}),
-            new Item(3250, "Berserker's Greaves - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new int[] {3006}),
-            new Item(3006, "Berserker's Greaves", Tier.Advanced, 225, 700, 0f, new int[] {1042, 1001}),
-            new Item(3144, "Bilgewater Cutlass", Tier.Legendary, 240, 980, 450f, new int[] {1036, 1053}),
-            new Item(3188, "Blackfire Torch", Tier.Legendary, 970, 1855, 0f, new int[] {1026, 3108}),
-            new Item(3153, "Blade of the Ruined King", Tier.Mythical, 900, 2240, 450f, new int[] {3144, 1042, 1042}),
+            new Item(3060, "Banner of Command", Tier.Legendary, 280, 2100, 0f, new[] { 3105, 3108 }),
+            new Item(3102, "Banshee's Veil", Tier.Legendary, 1150, 1925, 0f, new[] { 3211, 1028 }),
+            new Item(3254, "Berserker's Greaves - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new[] { 3006 }),
+            new Item(3251, "Berserker's Greaves - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new[] { 3006 }),
+            new Item(
+                3253, "Berserker's Greaves - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new[] { 3006 }),
+            new Item(3252, "Berserker's Greaves - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new[] { 3006 }),
+            new Item(3250, "Berserker's Greaves - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new[] { 3006 }),
+            new Item(3006, "Berserker's Greaves", Tier.Advanced, 225, 700, 0f, new[] { 1042, 1001 }),
+            new Item(3144, "Bilgewater Cutlass", Tier.Legendary, 240, 980, 450f, new[] { 1036, 1053 }),
+            new Item(3188, "Blackfire Torch", Tier.Legendary, 970, 1855, 0f, new[] { 1026, 3108 }),
+            new Item(3153, "Blade of the Ruined King", Tier.Mythical, 900, 2240, 450f, new[] { 3144, 1042, 1042 }),
             new Item(1026, "Blasting Wand", Tier.Basic, 860, 602, 0f),
             new Item(3166, "Bonetooth Necklace", Tier.RengarsTrinket, 0, 0, 0f),
             new Item(3167, "Bonetooth Necklace", Tier.RengarsTrinket, 0, 0, 0f),
@@ -73,30 +173,30 @@ namespace LeagueLib
             new Item(3452, "Bonetooth Necklace", Tier.RengarsTrinket, 0, 0, 0f),
             new Item(3453, "Bonetooth Necklace", Tier.RengarsTrinket, 0, 0, 0f),
             new Item(3454, "Bonetooth Necklace", Tier.RengarsTrinket, 0, 0, 0f),
-            new Item(3274, "Boots of Mobility - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new int[] {3117}),
-            new Item(3271, "Boots of Mobility - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new int[] {3117}),
-            new Item(3273, "Boots of Mobility - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new int[] {3117}),
-            new Item(3272, "Boots of Mobility - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new int[] {3117}),
-            new Item(3270, "Boots of Mobility - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new int[] {3117}),
-            new Item(3117, "Boots of Mobility", Tier.Enchantment, 475, 560, 0f, new int[] {1001}),
+            new Item(3274, "Boots of Mobility - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new[] { 3117 }),
+            new Item(3271, "Boots of Mobility - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new[] { 3117 }),
+            new Item(3273, "Boots of Mobility - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new[] { 3117 }),
+            new Item(3272, "Boots of Mobility - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new[] { 3117 }),
+            new Item(3270, "Boots of Mobility - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new[] { 3117 }),
+            new Item(3117, "Boots of Mobility", Tier.Enchantment, 475, 560, 0f, new[] { 1001 }),
             new Item(1001, "Boots of Speed", Tier.Basic, 325, 227, 0f),
-            new Item(3284, "Boots of Swiftness - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new int[] {3009}),
-            new Item(3281, "Boots of Swiftness - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new int[] {3009}),
-            new Item(3283, "Boots of Swiftness - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new int[] {3009}),
-            new Item(3282, "Boots of Swiftness - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new int[] {3009}),
-            new Item(3280, "Boots of Swiftness - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new int[] {3009}),
-            new Item(3009, "Boots of Swiftness", Tier.Advanced, 675, 700, 0f, new int[] {1001}),
+            new Item(3284, "Boots of Swiftness - Enchantment: Alacrity", Tier.Enchantment, 475, 0, 0f, new[] { 3009 }),
+            new Item(3281, "Boots of Swiftness - Enchantment: Captain", Tier.Enchantment, 600, 0, 0f, new[] { 3009 }),
+            new Item(3283, "Boots of Swiftness - Enchantment: Distortion", Tier.Enchantment, 475, 0, 0f, new[] { 3009 }),
+            new Item(3282, "Boots of Swiftness - Enchantment: Furor", Tier.Enchantment, 475, 0, 0f, new[] { 3009 }),
+            new Item(3280, "Boots of Swiftness - Enchantment: Homeguard", Tier.Enchantment, 475, 0, 0f, new[] { 3009 }),
+            new Item(3009, "Boots of Swiftness", Tier.Advanced, 675, 700, 0f, new[] { 1001 }),
             new Item(1051, "Brawler's Gloves", Tier.Basic, 400, 280, 0f),
-            new Item(3010, "Catalyst the Protector", Tier.Advanced, 400, 840, 0f, new int[] {1028, 1027}),
-            new Item(1031, "Chain Vest", Tier.Basic, 750, 504, 0f, new int[] {1029}),
-            new Item(3028, "Chalice of Harmony", Tier.Advanced, 140, 700, 0f, new int[] {1004, 1004, 1033}),
+            new Item(3010, "Catalyst the Protector", Tier.Advanced, 400, 840, 0f, new[] { 1028, 1027 }),
+            new Item(1031, "Chain Vest", Tier.Basic, 750, 504, 0f, new[] { 1029 }),
+            new Item(3028, "Chalice of Harmony", Tier.Advanced, 140, 700, 0f, new[] { 1004, 1004, 1033 }),
             new Item(1018, "Cloak of Agility", Tier.Basic, 730, 511, 0f),
             new Item(1029, "Cloth Armor", Tier.Basic, 300, 210, 0f),
-            new Item(3801, "Crystalline Bracer", Tier.Advanced, 600, -1, 0f, new int[] {1028, 1006}),
+            new Item(3801, "Crystalline Bracer", Tier.Advanced, 600, -1, 0f, new[] { 1028, 1006 }),
             new Item(2041, "Crystalline Flask", Tier.Consumable, 345, 138, 0f),
             new Item(1042, "Dagger", Tier.Basic, 450, 315, 0f),
-            new Item(3128, "Deathfire Grasp", Tier.Legendary, 680, 2170, 750f, new int[]{1058, 3108}),
-            new Item(3137, "Dervish Blade", Tier.Legendary, 200, 2100, 0f, new int[] {3140, 3101}),
+            new Item(3128, "Deathfire Grasp", Tier.Legendary, 680, 2170, 750f, new[] { 1058, 3108 }),
+            new Item(3137, "Dervish Blade", Tier.Legendary, 200, 2100, 0f, new[] { 3140, 3101 }),
             new Item(1075, "Doran's Blade (Showdown)", Tier.Basic, 440, 176, 0f),
             new Item(1055, "Doran's Blade", Tier.Basic, 440, 176, 0f),
             new Item(1076, "Doran's Ring (Snowdown)", Tier.Basic, 400, 160, 0f),
@@ -109,92 +209,12 @@ namespace LeagueLib
             new Item(2137, "Elixir of Ruin", Tier.Consumable, 400, -1, 0f),
             new Item(2139, "Elixir of Sorcery", Tier.Consumable, 400, -1, 0f),
             new Item(2140, "Elixir of Wrath", Tier.Consumable, 400, -1, 0f),
-            new Item(3184, "Entropy", Tier.Legendary, 500, 1890, 0f, new int[] {3044,1037}),
-            new Item(3508, "Essence Reaver", Tier.Legendary, 850, 2240, 0f, new int[] {1038,1053}),
+            new Item(3184, "Entropy", Tier.Legendary, 500, 1890, 0f, new[] { 3044, 1037 }),
+            new Item(3508, "Essence Reaver", Tier.Legendary, 850, 2240, 0f, new[] { 1038, 1053 }),
             new Item(1054, "Doran's Shield", Tier.Basic, 440, 176, 0f),
+            new Item(2003, "Health Potion", Tier.Consumable, 35, 15, 0f)
         };
+
         private static readonly List<Item> items = new List<Item>(pre_items);
-
-        public static Item GetItemByName(string name)
-        {
-            return items.FirstOrDefault(i => i.GetName().Equals(name));
-        }
-
-        public static Item GetItem(int itemID)
-        {
-            return items.FirstOrDefault(i => i.GetId() == itemID);
-        }
-
-        public class Item
-        {
-            private readonly int[] components;
-            private readonly int itemID;
-            private readonly string itemName;
-            private readonly float itemRange;
-            private readonly Tier itemTier;
-            private readonly int priceValue;
-            private readonly int sellValue;
-
-            public Item(int itemID, string itemName, Tier itemTier, int priceValue, int sellValue, float itemRange, int[] ucomponents = null)
-            {
-                this.itemID = itemID;
-                this.itemName = itemName;
-                this.itemTier = itemTier;
-                this.priceValue = priceValue;
-                this.sellValue = sellValue;
-                this.itemRange = itemRange;
-                this.components = ucomponents ?? new int[0];
-            }
-
-            public int GetId()
-            {
-                return itemID;
-            }
-
-            public ItemId GetItemId()
-            {
-                return (ItemId)itemID;
-            }
-
-            public string GetName()
-            {
-                return itemName;
-            }
-
-            public Tier GetTier()
-            {
-                return itemTier;
-            }
-
-            public int GetFullPriceValue()
-            {
-                return priceValue + components.SelectMany(i => items.Where(c => c.GetId() == i)).Sum(i2 => i2.GetPriceValue());
-            }
-
-            public int GetPriceValue()
-            {
-                return priceValue;
-            }
-
-            public int GetSellValue()
-            {
-                return sellValue;
-            }
-
-            public float GetRange()
-            {
-                return itemRange;
-            }
-
-            public int[] GetComponents()
-            {
-                return components;
-            }
-
-            public override string ToString()
-            {
-                return itemName + "[" + itemID + "]";
-            }
-        }
     }
 }
--- LeagueLib/LeagueLib/ShopAI.cs
+++ LeagueLib/LeagueLib/ShopAI.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
+using LeagueSharp.Common;
 
 #endregion
 
@@ -12,123 +13,191 @@ namespace LeagueLib
 {
     public class ShopAI
     {
-        private readonly Hashtable shop;
-        private int shopIndex;
         private const int maxShop = 10;
+        private int shopIndex;
+        private readonly Hashtable shop;
 
         public ShopAI(Hashtable shop = null, int shopIndex = int.MaxValue)
         {
             this.shop = shop ?? new Hashtable();
 
+            if (shop == null)
+            {
+                return;
+            }
+
             if (shop.Count > 0 && shop.Count < maxShop)
+            {
                 this.shopIndex = shop.Count;
+            }
             else if (shop.Count < maxShop)
+            {
                 this.shopIndex = 0;
+            }
             else
+            {
                 this.shopIndex = maxShop;
+            }
         }
 
         public int AddItem(int itemId)
         {
             Console.WriteLine("debug: " + Items.GetItem(itemId));
-            return this.AddItem(new ShopItem(Items.GetItem(itemId)));
+            return AddItem(new ShopItem(Items.GetItem(itemId)));
         }
 
         public int AddItem(ItemId itemId)
         {
-            Console.WriteLine("debug: " + Items.GetItem((int)itemId));
-            return this.AddItem(new ShopItem(Items.GetItem((int)itemId)));
+            Console.WriteLine("debug: " + Items.GetItem((int) itemId));
+            ShopItem shopitem;
+            try
+            {
+                shopitem = new ShopItem(Items.GetItem((int) itemId));
+            }
+            catch
+            {
+                Console.WriteLine(itemId.ToString() + " failed!");
+                return -1;
+            }
+            return AddItem(shopitem);
         }
 
         public int AddItem(string itemName)
         {
             Console.WriteLine("debug: " + Items.GetItemByName(itemName));
-            return this.AddItem(new ShopItem(Items.GetItemByName(itemName)));
+            return AddItem(new ShopItem(Items.GetItemByName(itemName)));
         }
 
         public int AddItem(ShopItem shopItem)
         {
             if (shop.ContainsKey(shopIndex) && shopIndex < maxShop)
-                GetFreeIndex();
-
-            if(shopIndex < maxShop)
             {
-                shop.Add(shopIndex, shopItem);
-                ++shopIndex;
-                return shopIndex;
+                GetFreeIndex();
             }
 
-            return -1;
+            if (shopIndex >= maxShop)
+            {
+                return -1;
+            }
+
+            shop.Add(shopIndex, shopItem);
+            ++shopIndex;
+            return shopIndex;
+        }
+
+        public void AddItemList(List<ItemId> items)
+        {
+            foreach (var item in items)
+            {
+                AddItem(item);
+            }
         }
 
         public bool RemoveItem(ShopItem shopItem)
         {
-            if (shop.ContainsValue(shopItem))
-                foreach (var i in shop.Keys)
-                    if (shop[i].Equals(shopItem))
-                        return RemoveItem((int)i);
-
-            return false;
+            return shop.ContainsValue(shopItem) &&
+                   (from object i in shop.Keys where shop[i].Equals(shopItem) select RemoveItem((int) i)).FirstOrDefault
+                       ();
         }
 
-        public bool RemoveItem(int shopIndex)
+        public bool RemoveItem(int shopindex)
         {
-            if (shopIndex < maxShop && shop.ContainsKey(shopIndex))
+            if (shopindex >= maxShop || !shop.ContainsKey(shopindex))
             {
-                shop.Remove(shopIndex);
-                return true;
+                return false;
             }
-            return false;
+            shop.Remove(shopindex);
+            return true;
         }
 
         private int GetFreeIndex()
         {
-            for(var c = 0; c < maxShop; ++c)
+            for (var c = 0; c < maxShop; ++c)
+            {
                 if (!shop.ContainsKey(c))
+                {
                     return c;
+                }
+            }
 
             return maxShop;
+        }
+
+        public void Shop()
+        {
+            foreach (var item in shop.Values.Cast<ShopItem>().Where(item => !item.IsProtected()))
+            {
+                item.Buy();
+            }
         }
     }
 
     public class ShopItem
     {
+        private bool itemBought;
+        private Command itemOrder;
+        private readonly Items.Item Item;
+        private readonly List<Items.Item> itemComponents;
+        private readonly int itemFullPrice;
+        private readonly ItemId itemId;
+        private readonly int itemPrice;
+
+        public ShopItem(Items.Item item)
+        {
+            Item = item;
+            itemId = (ItemId) item.GetId();
+            itemFullPrice = item.GetFullPriceValue();
+            itemPrice = item.GetPriceValue();
+            itemComponents = item.GetComponentsList();
+        }
+
+        public void Buy()
+        {
+            if (itemBought)
+            {
+                return;
+            }
+
+            var gold = ObjectManager.Player.GoldCurrent;
+
+            // can afford full item
+            if (gold >= itemFullPrice)
+            {
+                Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+                Item.Buy();
+                return;
+            }
+
+            // buy components
+            foreach (var item in itemComponents.Where(item => ObjectManager.Player.Gold > item.GetPriceValue()))
+            {
+                item.Buy();
+            }
+        }
+
+        private void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] != Packet.S2C.BuyItemAns.Header ||
+                Packet.S2C.BuyItemAns.Decoded(args.PacketData).Item.Id != Item.GetId())
+            {
+                return;
+            }
+            Console.WriteLine("bought item!");
+            itemBought = true;
+        }
+
+        public void Sell() { }
+
+        public bool IsProtected()
+        {
+            return itemBought;
+        }
+
         private enum Command
         {
             Buy,
             Sell,
             Idle
-        }
-
-        private readonly ItemId itemId;
-        private readonly int itemFullPrice;
-        private readonly int itemPrice;
-        private readonly int[] itemComponents;
-
-        private bool itemBought;
-        private Command itemOrder;
-
-        public ShopItem(Items.Item item)
-        {
-            itemId = (ItemId)item.GetId();
-            itemFullPrice = item.GetFullPriceValue();
-            itemPrice = item.GetPriceValue();
-            itemComponents = item.GetComponents();
-        }
-
-        public void Buy()
-        {
-            
-        }
-
-        public void Sell()
-        {
-
-        }
-
-        public bool IsProtected()
-        {
-            return itemBought ? true : false;
         }
     }
 }
